a89c2924fe39da02f2228425451da12b1fafa61e
unknown <Administrator@vendan-desktop.(none)>
2012-02-12 00:06:33 -0500

    Initial Commit

---
+++ SevenZipSharp/SevenZip.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+using SevenZip;
+using SevenZip.Compression.LZMA;
+
+namespace SevenZip
+{
+    public class LZMAEncoderStream : Stream
+    {
+        private int _lzmaDictionarySize;
+        private LzmaCycleEncoder encoder;
+        private Stream _baseStream;
+        private bool _streamOwner;
+        private CircularStream tmpBuffer;
+
+        public int LzmaDictionarySize
+        {
+            get
+            {
+                return _lzmaDictionarySize;
+            }
+            set
+            {
+                _lzmaDictionarySize = value;
+            }
+        }
+
+        public LZMAEncoderStream() : this(new MemoryStream())
+        {
+        }
+
+        public LZMAEncoderStream(Stream baseStream) : this(baseStream, true, 1<<15)
+        {
+        }
+
+        public LZMAEncoderStream(Stream baseStream, bool streamOwner, int buffersize)
+        {
+            _baseStream = baseStream;
+            _lzmaDictionarySize = 22;
+            encoder = new LzmaCycleEncoder();
+            WriteLzmaProperties(encoder);
+            _streamOwner = streamOwner;
+            tmpBuffer = new CircularStream(buffersize);
+            encoder.WriteCoderProperties(_baseStream);
+            encoder.Start(tmpBuffer, baseStream);
+        }
+
+        internal void WriteLzmaProperties(LzmaCycleEncoder encoder)
+        {
+            CoderPropID[] propIDs =
+                {
+                    CoderPropID.DictionarySize,
+                    CoderPropID.PosStateBits,
+                    CoderPropID.LitContextBits,
+                    CoderPropID.LitPosBits,
+                    CoderPropID.Algorithm,
+                    CoderPropID.NumFastBytes,
+                    CoderPropID.MatchFinder,
+                    CoderPropID.EndMarker
+                };
+            object[] properties =
+                {
+                    _lzmaDictionarySize,
+                    2,
+                    3,
+                    0,
+                    2,
+                    256,
+                    "bt4",
+                    true
+                };
+            encoder.SetCoderProperties(propIDs, properties);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            int totalWrite = count;
+            int readPos = 0;
+
+            while (totalWrite > 0)
+            {
+                int writeAmt = Math.Min((int)(tmpBuffer.Capacity - tmpBuffer.Length), totalWrite);
+                tmpBuffer.Write(buffer, offset + readPos, writeAmt);
+                totalWrite -= writeAmt;
+                readPos += writeAmt;
+                encoder.Code();
+            }
+
+        }
+
+        public void WritePlain(byte[] buffer, int offset, int count)
+        {
+            _baseStream.Write(buffer, offset, count);
+        }
+
+        public override bool CanRead { get { return false; } }
+        public override bool CanWrite { get { return true; } }
+        public override bool CanSeek { get { return false; } }
+
+        public override void Flush()
+        {
+            encoder.Finish();
+        }
+
+        public override long Length
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                return 0;
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Close()
+        {
+            _baseStream.Close();
+            base.Close();
+        }
+    }
+
+    public class LZMADecoderStream : Stream
+    {
+        private LzmaCycleDecoder decoder;
+        private Stream _baseStream;
+        private bool _streamOwner;
+        private CircularStream tmpBuffer;
+        Int64 readPos;
+
+        public LZMADecoderStream(Stream baseStream)
+            : this(baseStream, true, 1<<15)
+        {
+        }
+
+        public LZMADecoderStream(Stream baseStream, bool streamOwner, int buffersize)
+        {
+            _baseStream = baseStream;
+            tmpBuffer = new CircularStream(buffersize);
+            decoder = new LzmaCycleDecoder();
+            decoder.SetDecoderProperties(GetLzmaProperties(baseStream));
+            decoder.Start(baseStream, tmpBuffer);
+            readPos = 0;
+            _streamOwner = streamOwner;
+        }
+
+        internal byte[] GetLzmaProperties(Stream inStream)
+        {
+            byte[] LZMAProps = new byte[5];
+            inStream.Read(LZMAProps, 0, 5);
+            return LZMAProps;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanRead { get { return true; } }
+        public override bool CanWrite { get { return false; } }
+        public override bool CanSeek { get { return false; } }
+
+        public override void Flush()
+        {
+
+        }
+
+        public override long Length
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                return 0;
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            int totalRead = count;
+            int amtRead = 0;
+            while (totalRead > 0)
+            {
+                int toDecode = Math.Min((int)(tmpBuffer.Capacity - tmpBuffer.Length) - tmpBuffer.Capacity / 2, totalRead);
+                toDecode = Math.Max(toDecode, 0);
+                decoder.Code(readPos + toDecode);
+                int amt = tmpBuffer.Read(buffer, offset + amtRead, count);
+                readPos = readPos + amt;
+                totalRead = totalRead - amt;
+                amtRead += amt;
+                if (amt == 0)
+                    return amtRead;
+            }
+            return amtRead;
+        }
+
+        public int ReadPlain(byte[] buffer, int offset, int count)
+        {
+            return _baseStream.Read(buffer, offset, count);
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Close()
+        {
+            this.Flush();
+            _baseStream.Close();
+            base.Close();
+        }
+    }
+}
---
+++ SevenZipTest/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SevenZip;
+using System.IO;
+using System.Diagnostics;
+
+namespace sevemziptest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            LZMAEncoderStream outStream = new LZMAEncoderStream(File.Create("allme.lz"));
+
+            FileStream input = File.Open("all.7z", FileMode.Open);
+            
+            byte[] inputBuf = new byte[1000];
+            int readAmt;
+
+            byte[] len = new byte[8];
+
+            for (int i = 0; i < 8; i++)
+                len[i] = (byte) (input.Length >> (8*i));
+
+            outStream.WritePlain(len, 0, 8);
+
+            while ((readAmt = input.Read(inputBuf, 0, 1000)) != 0)
+            {
+                outStream.Write(inputBuf, 0, readAmt);
+            }
+
+            outStream.Flush();
+            outStream.Close();
+            input.Close();
+            sw.Stop();
+
+            Debug.Print(string.Format("Time: {0}", sw.ElapsedMilliseconds));
+
+
+            LZMADecoderStream inStream = new LZMADecoderStream(File.Open("allme.lz", FileMode.Open));
+
+            len = new byte[8];
+            Int64 fileLen = 0;
+
+            inStream.ReadPlain(len, 0, 8);
+
+            for (int i = 0; i < 8; i++)
+                fileLen |= (((long)(byte)len[i] << (8 * i)));
+            
+            byte[] wholeBuf = new byte[fileLen];
+
+            sw.Restart();
+            inStream.Read(wholeBuf, 0, (int)fileLen);
+            sw.Stop();
+
+            File.WriteAllBytes("all2.7z", wholeBuf);
+
+            inStream.Close();
+
+
+            Debug.Print(string.Format("Time: {0}", sw.ElapsedMilliseconds));
+
+        }
+    }
+}
